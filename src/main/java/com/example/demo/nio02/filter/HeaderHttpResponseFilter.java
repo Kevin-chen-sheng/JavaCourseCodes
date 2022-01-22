8f42f82e8186032af5edd14aa71b467c2c9dbf3f
chends4 <chends4@chinatelecom.cn>
2022-01-22 19:17:51 +0800

    第三周作业

---
+++ src/main/java/com/example/demo/nio02/filter/HeaderHttpResponseFilter.java
@@ -0,0 +1,10 @@
+package com.example.demo.nio02.filter;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+
+public class HeaderHttpResponseFilter implements HttpResponseFilter {
+    @Override
+    public void filter(FullHttpResponse response) {
+        response.headers().set("kk", "dada");
+    }
+}
---
+++ src/main/java/com/example/demo/nio02/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,4 @@
+package com.example.demo.nio02.outbound.okhttp;
+
+public class OkhttpOutboundHandler {
+}
